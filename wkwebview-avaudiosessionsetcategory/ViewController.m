0da3c93bd318ac40415ca08d9426d707c0fc48bb
Tom Sullivan <tom@msbit.com.au>
2017-06-05 17:35:25 +1000

    formatting: align HTML text

--- wkwebview-avaudiosessionsetcategory/ViewController.m
+++ wkwebview-avaudiosessionsetcategory/ViewController.m
@@ -19,12 +19,12 @@
           <title>HTTP Live Streaming Example</title>\
         </head>\
         <body>\
-        <video\
-          src=\"http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8\"\
-          height=\"300\" width=\"400\">\
-        </video>\
-      </body>\
-    </html>" baseURL:nil];
+          <video\
+            src=\"http://devimages.apple.com/iphone/samples/bipbop/bipbopall.m3u8\"\
+            height=\"300\" width=\"400\">\
+          </video>\
+        </body>\
+      </html>" baseURL:nil];
   [self.view addSubview:wb];
   NSString *category = [[AVAudioSession sharedInstance] category];
   NSLog(@"category: %@\n", category);
